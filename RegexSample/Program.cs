043a36d6e9a0557ca6153ea0a39602386f1c62f8
ygrek <ygreksh@gmail.com>
2021-09-06 13:47:17 +0300

    1, 2, 3 - ready

--- RegexSample/Program.cs
+++ RegexSample/Program.cs
@@ -9,13 +9,13 @@ namespace RegexSample
         {
             string input1 = "1, 1000, 1 000 000, 100.23";
             string input2 = "http://ya.ru/api?r=1&x=23";
-            string input3 = "";
+            string input3 = "В  этом тексте должны быть удалены  лишние   пробелы";
             string[] values = new[] { "123", "+373 77767852", "3333", "77767852", "0 (777) 67852", "-33377088" };
 
             string pattern1 = @"\b100\.\d{2}|10{3}|(1 0{3} 0{3})|1\b";
             string pattern2 = @"(?<=api\?)\S+?(?=&)|(?<=&)\S+?(?=&)|(?<=&)\S+";
-            string pattern3 = "";
-            string pattern4 = "";
+            string pattern3 = @"[ ]+";
+            string pattern4 = @"";
 
             Console.WriteLine(input1);
             MatchCollection matchCollection = Regex.Matches(input1, pattern1);
@@ -29,6 +29,10 @@ namespace RegexSample
             {
                 Console.WriteLine("{0}' found at position {1}.", match.Value, match.Index);
             }
+            
+            Console.WriteLine(input3);
+            string replacement = " ";
+            Console.WriteLine(Regex.Replace(input3,pattern3,replacement));
 
         }
     }
